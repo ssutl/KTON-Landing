627949204e66c97f192f655db5fc23e074c12c4c
Shay Campbell <shayc.campbell@gmail.com>
2023-08-15 22:52:03 +0100

    Roadmap styling

--- src/pages/Membership/index.tsx
+++ src/pages/Membership/index.tsx
@@ -1,20 +1,13 @@
-import Navbar from "@/Components/Navbar";
 import MembershipCard from "../../Components/MembershipCard";
 import styles from "../../styles/MembershipPage.module.scss";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import axios from "axios";
 
 const Membership = () => {
   return (
-    <>
-      <div className={styles.membershipPage}>
-        <MembershipCard type="Free" />
-        <MembershipCard type="Premium" />
-      </div>
-    </>
+    <div className={styles.membershipPage}>
+      <MembershipCard type="Free" />
+      <MembershipCard type="Premium" />
+    </div>
   );
-
-  return null;
 };
 
 export default Membership;
