71cac4e056a08c821cdfd09aec57ed9f2478b3f8
Shay Campbell <shayc.campbell@gmail.com>
2023-08-15 12:32:22 +0100

    Fixing bug

--- src/pages/Membership/index.tsx
+++ src/pages/Membership/index.tsx
@@ -4,32 +4,9 @@ import styles from "../../styles/MembershipPage.module.scss";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import axios from "axios";
 
-export const getStaticProps: GetStaticProps<{
-  total: number;
-}> = async (context) => {
-  const response = await axios({
-    method: "GET",
-    url: `${process.env.NEXT_PUBLIC_BACKENDURL}/total-highlights`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  console.log("response: ", response.data);
-
-  return {
-    props: {
-      total: response.data,
-    },
-  };
-};
-
-const Membership = ({
-  total,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Membership = () => {
   return (
     <>
-      <Navbar total={total.toLocaleString()} />
       <div className={styles.membershipPage}>
         <MembershipCard type="Free" />
         <MembershipCard type="Premium" />
