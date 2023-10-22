2a0f4b6cae926951af0078a04f4450c4b125bba0
c.j.c <curtisjcampbell@mail.com>
2023-10-22 23:05:01 +0100

    refactors membership card styling and content

--- src/Components/MembershipCard.tsx
+++ src/Components/MembershipCard.tsx
@@ -1,78 +1,110 @@
 import styles from "../styles/MembershipCard.module.scss";
-import { useRouter } from "next/router";
 import React, { useState, useEffect, useContext } from "react";
 
 interface MembershipCardProps {
-  type: "Free" | "Premium";
+	type: "Free" | "Premium";
 }
 
+type Feature = {
+	description: string;
+	plan: MembershipCardProps["type"];
+};
+
 const MembershipCard = ({ type }: MembershipCardProps) => {
-  const router = useRouter();
+	const [billing_type, setBillingType] = useState<"Monthly" | "Anually">("Monthly");
+	const [price, setPrice] = useState(0);
 
-  const [billing_type, setBillingType] = useState<"Monthly" | "Anually">(
-    "Monthly"
-  );
-  const [price, setPrice] = useState(0);
+	useEffect(() => {
+		if (type === "Premium") {
+			if (billing_type === "Monthly") {
+				setPrice(3);
+			} else {
+				setPrice(30);
+			}
+		}
+	}, [billing_type, type]);
 
-  useEffect(() => {
-    if (type === "Premium") {
-      if (billing_type === "Monthly") {
-        setPrice(3);
-      } else {
-        setPrice(30);
-      }
-    }
-  }, [billing_type]);
+	const platformFeatures: Feature[] = [
+		{
+			description: "Categorise books using genre tags",
+			plan: "Free",
+		},
+		{
+			description: "Reading metrics and insights",
+			plan: "Free",
+		},
+		{
+			description: "Annotate, favourite and tag highlights",
+			plan: "Free",
+		},
+		{
+			description: "Share highlights as images",
+			plan: "Free",
+		},
+		{
+			description: "Add custom book covers",
+			plan: "Free",
+		},
+		{
+			description: "Export highlights to Notion",
+			plan: "Premium",
+		},
+		{
+			description: "Export highlights as CSV",
+			plan: "Premium",
+		},
+		{
+			description: "No image watermarks when sharing",
+			plan: "Premium",
+		},
+	];
 
-  const freeFeatures = [
-    "Everything in preview plan, without book or highlight limits",
-    "Categorise books using genre tags",
-    "Organise your highlights by favouriting, tagging & adding notes",
-    "Add custom book covers",
-  ];
-
-  const premiumFeatures = [
-    "Everything in free plan",
-    "No image watermarks",
-    "Various integrations including Notion & Anki",
-    "Detailed analytics & insights in the form of charts & graphs",
-  ];
-
-  const features = type === "Free" ? freeFeatures : premiumFeatures;
-
-  return (
-    <div className={`${styles.membershipCard}`}>
-      <div className={styles.membershipCardHeader}>
-        <h2>{type}</h2>
-        {type === "Premium" && (
-          <p
-            onClick={(e) => {
-              e.stopPropagation();
-              if (billing_type === "Monthly") {
-                setBillingType("Anually");
-              } else {
-                setBillingType("Monthly");
-              }
-            }}
-          >
-            Prefer to pay {billing_type === "Anually" ? "monthly" : "anually"}?{" "}
-            <span>Click here</span>
-          </p>
-        )}
-      </div>
-      <div className={styles.membershipCardPrice}>
-        <h1>${price}</h1>
-        <h2>/{billing_type}</h2>
-      </div>
-      <div className={styles.membershipCardFeatures}>
-        {features.map((feature, index) => (
-          <div key={index} className={styles.featureItem}>
-            <h3>✅ {feature}</h3>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${styles.membershipCard}`}>
+			<div className={styles.membershipCardHeader}>
+				<h2>{type}</h2>
+				{type === "Premium" && (
+					<p
+						onClick={(e) => {
+							e.stopPropagation();
+							if (billing_type === "Monthly") {
+								setBillingType("Anually");
+							} else {
+								setBillingType("Monthly");
+							}
+						}}
+					>
+						Prefer to pay {billing_type === "Anually" ? "monthly" : "anually"}? <span>Click here</span>
+					</p>
+				)}
+			</div>
+			<div className={styles.membershipCardPrice}>
+				<h1>${price}</h1>
+				<h2>/{billing_type}</h2>
+			</div>
+			<div className={styles.membershipCardFeatures}>
+				{platformFeatures.map(({ description, plan }, index) => {
+					const isNotFreeTier = type === "Free" && plan !== "Free";
+					return (
+						<div key={index} className={styles.featureItem}>
+							<h3
+								style={
+									isNotFreeTier
+										? { color: "grey", textDecoration: "line-through", textDecorationThickness: "0.5px" }
+										: { color: "white" }
+								}
+							>
+								<span style={isNotFreeTier ? { color: "grey" } : { color: "#3fcdfc" }}>
+									{isNotFreeTier ? "✗ " : "✓ "}
+								</span>
+								{description}
+							</h3>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default MembershipCard;
