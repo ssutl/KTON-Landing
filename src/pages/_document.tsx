f7388ae78dc016995333c0ba0872ac8d4ba371a4
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-11-03 18:59:03 +0000

    Updating description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,54 +1,119 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head>
-				<meta
-					name="description"
-					content="Organise, annotate and share your highlights from this all-in-one clipppings.txt management platform 📖📝"
-				/>
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="description"
+          content="An all in one highlight manager for your e-books. We‘ll help you organise, annotate and export your kindle highlights. Join us with a free forever account and then optionally upgrade your plan to unlock more features. Finding your highlights made easy, happy reading with KTON! 📖📝"
+        />
 
-				<meta name="google-site-verification" content="5rYKI5qv54lywaICk-K65AEQNxKGIbHYwO0IdAsLVCo" />
+        <meta
+          name="google-site-verification"
+          content="5rYKI5qv54lywaICk-K65AEQNxKGIbHYwO0IdAsLVCo"
+        />
 
-				<meta property="og:url" content="https://www.app.kton.xyz/" />
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="KTON: Kindle Highlight Manager" />
-				<meta
-					property="og:description"
-					content="Organise, annotate and share your highlights from this all-in-one clipppings.txt management platform 📖📝"
-				/>
-				<meta property="og:image" content="https://app.kton.xyz/images/SEO-image.png" />
+        <meta property="og:url" content="https://www.app.kton.xyz/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="KTON: Kindle Highlight Manager" />
+        <meta
+          property="og:description"
+          content="An all in one highlight manager for your e-books. We‘ll help you organise, annotate and export your kindle highlights. Join us with a free forever account and then optionally upgrade your plan to unlock more features. Finding your highlights made easy, happy reading with KTON! 📖📝"
+        />
+        <meta
+          property="og:image"
+          content="https://app.kton.xyz/images/SEO-image.png"
+        />
 
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta property="twitter:domain" content="kton.xyz" />
-				<meta property="twitter:url" content="https://www.app.kton.xyz/" />
-				<meta name="twitter:title" content="KTON: Kindle Highlight Manager" />
-				<meta
-					name="twitter:description"
-					content="Organise, annotate and share your highlights from this all-in-one clipppings.txt management platform 📖📝"
-				/>
-				<meta name="twitter:image" content="https://app.kton.xyz/images/SEO-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="kton.xyz" />
+        <meta property="twitter:url" content="https://www.app.kton.xyz/" />
+        <meta name="twitter:title" content="KTON: Kindle Highlight Manager" />
+        <meta
+          name="twitter:description"
+          content="An all in one highlight manager for your e-books. We‘ll help you organise, annotate and export your kindle highlights. Join us with a free forever account and then optionally upgrade your plan to unlock more features. Finding your highlights made easy, happy reading with KTON! 📖📝"
+        />
+        <meta
+          name="twitter:image"
+          content="https://app.kton.xyz/images/SEO-image.png"
+        />
 
-				<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-				<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-				<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-				<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-				<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-				<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-				<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-				<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-				<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/manifest.json"></link>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
